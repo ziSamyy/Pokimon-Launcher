ae989a9b0e1fc47de1f21312296fd7de809dfe75
Dev4lexG <alexglezfdez05@gmail.com>
2023-07-11 22:17:04 +0200

    Filx publish

--- build.js
+++ build.js
@@ -3,7 +3,8 @@ const nodeFetch = require('node-fetch')
 const fs = require('fs')
 const png2icons = require('png2icons')
 const Jimp = require('jimp')
-const { preductname } = require('./package.json')
+let { preductname } = require('./package.json')
+preductname = preductname.replace(' ', '-')
 
 class Index {
 	async build() {
